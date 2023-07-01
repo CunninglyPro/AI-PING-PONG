d2bc6c66fdb959aa0cee0017afed4c23ab2131b4
Aditya <99547519+CunninglyPro@users.noreply.github.com>
2023-07-01 20:45:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,500);
+  var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
   video = createCapture(VIDEO);
   video.size(700, 600);
